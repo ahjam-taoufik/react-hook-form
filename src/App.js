279ae0ea157c81537e317b0a84d95393a7290536
taoufik ahjam <taoufikahjam@gmail.com>
2022-02-18 15:24:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log('ok',data);
+
+  console.log(watch(["Lname","Fname"])); // watch input value by passing the name of it
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+
+    <form onSubmit={handleSubmit(onSubmit)}> <br/><br/>
+    
+      <input  {...register("Fname", { required: true })} /> <br/><br/>
+      {errors.Fname && <span>This field is required</span>}<br/>
+     
+      <input {...register("Lname", { required: true })} /><br/><br/>
+    
+      {errors.Lname && <span>This field is required</span>}<br/>
+      
+      <input type="submit" />
+    </form>
+   </div>
   );
 }
-
-export default App;
