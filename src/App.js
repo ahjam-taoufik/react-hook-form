cd2f8e4d753a09d844073131c23b57e9c510888a
taoufik ahjam <taoufikahjam@gmail.com>
2022-02-18 15:37:00 +0100

    value of number

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log('ok',data);
 
-  console.log(watch(["Lname","Fname"])); // watch input value by passing the name of it
-
+ 
   return (
    <div className='App'>
 
     <form onSubmit={handleSubmit(onSubmit)}> <br/><br/>
     
-      <input  {...register("Fname", { required: true })} /> <br/><br/>
-      {errors.Fname && <span>This field is required</span>}<br/>
+
+      <input  {...register("name", { required: true })} /> <br/><br/>
+      {errors.name && <span>This field is required</span>}<br/>
      
-      <input {...register("Lname", { required: true })} /><br/><br/>
-    
-      {errors.Lname && <span>This field is required</span>}<br/>
+
+      <input {...register("age", { required: true ,min:20,valueAsNumber:true })} /><br/><br/>
+      {errors.age && <span>This field is required or wrong</span>}<br/>
       
+
       <input type="submit" />
     </form>
    </div>
